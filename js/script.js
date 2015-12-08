ec831566b8fb08e04f06cbaa134611a9acb22006
4knort <4knort@gmail.com>
2015-12-08 16:53:49 +0200

    JavaScript

--- js/script.js
+++ js/script.js
@@ -1,9 +1,25 @@
 var navBtn = document.querySelector (".js-main-navigation-button");
-var mainNav = document.querySelector(".js-main-nav");
+var plus = document.querySelectorAll (".counter__btn--plus");
+var counterInput = document.querySelector (".counter__input");
+
+
+for(i=0;i<plus.length;++i)
+  plus[i].addEventListener("click", function(event){
+    event.preventDefault();
+    if(parseInt(counterInput.value) < 30){
+      counterInput.value = parseInt(counterInput.value) +1;
+    }
+  })
+
 
 
 navBtn.addEventListener("click", function(event){
   event.preventDefault(),
-  mainNav.classList.toggle("main-nav-open"),
   navBtn.classList.toggle("main-navigation-close")
 });
+
+$(document).ready(function(){
+    $(".js-main-navigation-button").click(function(){
+        $(".js-main-nav").slideToggle("slow");
+    })
+})
