09fc7fa78a75ae453c7ffe92d47928f928ee7577
4knort <4knort@gmail.com>
2015-12-14 23:10:31 +0200

    grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function(grunt) {
     less: {
       style: {
         files: {
-          "css/style.css": "less/style.less"
+          "build/css/style.css": "source/less/style.less"
         }
       }
     },
@@ -21,26 +21,111 @@ module.exports = function(grunt) {
         ]
       },
       style: {
-        src: "css/*.css"
+        src: "build/css/*.css"
       }
     },
 
     watch: {
       style: {
-        files: ["less/**/*.less"],
+        files: ["source/less/**/*.less"],
         tasks: ["less", "postcss"],
         options: {
           spawn: false,
           livereload: true
         }
       }
+    },
+
+    cmq: {
+      style: {
+        files: {
+          "build/css/style.css": ["build/css/style.css"]
+        }
+      }
+    },
+
+    cssmin: {
+      style: {
+        files: {
+          "build/css/style.min.css": ["build/css/style.css"]
+        }
+      }
+    },
+
+    csscomb: {
+      style: {
+        expand: true,
+        src: ["source/less/**/*.less"]
+      }
+    },
+
+    imagemin: {
+      images: {
+        options: {
+          optimizationLevel: 3
+        },
+        files: [{
+          expand: true,
+          src: ["build/img/**/*.{png, jpg, gif, svg}"]
+        }]
+      }
+    },
+
+    copy: {
+      build: {
+        files: [{
+          expand: true,
+          cwd: "source",
+          src: [
+            "img/**",
+            "index.html",
+            "form.html",
+            "blog.html",
+            "post.html"
+          ],
+          dest: "build"
+        }]
+      }
+    },
+
+    clean: {
+      build: ["build"]
+    },
+
+    concat: {
+      dist: {
+        src: ["source/js/jquery.js", "source/js/script.js"],
+        dest: "build/js/script.js"
+      }
+    },
+
+    uglify: {
+      my_target: {
+        files: {
+          "build/js/script.min.js": ["build/js/script.js"]
+        }
+      }
     }
   };
-  
+
+
 
 
   // Не редактируйте эту строку
   config = require("./.gosha")(grunt, config);
 
   grunt.initConfig(config);
+
+  grunt.registerTask("build", [
+    "clean",
+    "copy",
+    "csscomb",
+    "less",
+    "cmq",
+    "postcss",
+    "cssmin",
+    "imagemin",
+    "concat",
+    "uglify"
+  ]);
 };
